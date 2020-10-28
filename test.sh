cee68f3b59ce766b00b18914ab4ee9173ead3006
Albert Wu <wua@uber.com>
2020-10-28 11:24:49 -0700

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -ex 
 
 echo "inside test.sh"
+
+analyze \
+  --step foo \
+  --type exitcode \
+  --args '{"category":"infra", "subcategory": "foobar"}'
+  false
+
+echo "should never get here"
