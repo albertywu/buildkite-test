bc6256d0e315e3d6029e4eb425548a6f9d64637f
Albert Wu <wua@uber.com>
2020-10-29 16:15:48 -0700

    fix

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 echo "write failure classification to artifacts/analysis/failure"
 
-mkdir -p artifacts/analysis/failure
+mkdir -p artifacts/analysis
 echo "user_failure foobar" > artifacts/analysis/failure
 
 echo "infra/buildkite-webhook-proxy will now use this info to classify the build failure"
